afb1bfa143b8fdb58da9979dc167ac10aa3d1f77
lucasgaricoix <lucasgaricoix@hotmail.com>
2019-11-01 21:35:43 -0300

    Adicionado catch para as requisições

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const save = (data: any) => {
-  return axios.post(`http://localhost:3333/book`, data)
+  return axios.post(`https://santa-monica-server.herokuapp.com/book`, data)
 }
 
 export const load = () => {
-  return axios.get(`http://localhost:3333/book`)
+  return axios.get(`https://santa-monica-server.herokuapp.com/book`)
 }
